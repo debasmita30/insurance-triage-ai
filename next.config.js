623ec9091991eec605105bebf96d33422e104b4f
Debasmita Chatterjee <115386608+debasmita30@users.noreply.github.com>
2026-04-09 12:02:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000"],
-    },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
-
+ 
 module.exports = nextConfig;
